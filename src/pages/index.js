9d4a776000f1dbeae15dd4325b07348d0269eafb
David Leiva <david.leiva@ti-medi.com>
2021-09-15 18:49:51 +0200

    adding small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -182,9 +182,8 @@ const IndexPage = () => {
                     width="32"
                     height="32"
                     fill="currentColor"
-                    class="bi bi-envelope-fill"
+                    className="bi bi-envelope-fill me-3"
                     viewBox="0 0 16 16"
-                    className="me-3"
                   >
                     <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
                   </svg>
